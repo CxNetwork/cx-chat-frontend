fbf8c953bee909707ea339be73eff95687fd886f
Ciaran Langton <email@ciaranlangton.com>
2018-12-02 20:31:39 +0000

    Emote Menu Button

---
+++ src/components/EmoteMenu.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "../cxemote.css";
+
+class EmoteMenu extends Component {
+  state = {
+    filterText: ""
+  }
+
+  render() {
+    return (
+      <div className="emoteMenuRoot">
+        <h1>Hi</h1>
+      </div>
+    );
+  }
+};
+
+export default EmoteMenu;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import '../cxd.css';
 import settingsIcon from "../assets/baseline-settings-20px.svg";
 import peopleIcon from "../assets/baseline-people-24px.svg";
-import emoteIcon from "../assets/baseline-tag_faces-24px.svg";
+import EmoteIcon from "../assets/EmoteIcon";
+
+import EmoteMenu from "./EmoteMenu";
 
 //temp
 import adminBadge from "../assets/baseline-build-24px.svg";
 
 class Footer extends Component {
   state = {
-    inputText: ""
+    inputText: "",
+    emotesVisible: false
   }
+
+  constructor() {
+    super();
+
+    this.toggleEmotes = this.toggleEmotes.bind(this);
+  }
+  
+  toggleEmotes() {
+    this.setState({ emotesVisible: !this.state.emotesVisible });
+  }
+
   render() {
     return (
-      <div className="footer">
-        <div className="chatBar">
-          <input className="chatBarInput" placeholder="Send a message..."/>
-          <img className="emoteButton" src={emoteIcon}/>
-        </div>
-        <div className="chatExperience">
-          <div className="optionButtons">
-            <img className="optionButton" src={settingsIcon}/>
-            <img className="optionButton" src={peopleIcon}/>
+      <Fragment>
+        <div className="footer">
+          <div className="chatBar">
+            <input className="chatBarInput" placeholder="Send a message..."/>
+            <button className="iconButton emoteButton" onClick={this.toggleEmotes}>
+              <EmoteIcon color={this.state.emotesVisible ? '#FFFFFF' : '#707070'}/>
+            </button>
           </div>
-          <div className="chatUserDisplay">
-            <p style={{color: "#ED0000", fontFamily: "Open Sans", margin: 0}}>Phineas</p>
-            <img style={{width: 18, marginLeft: 5}} src={adminBadge}/>
+          <div className="chatExperience">
+            <div className="optionButtons">
+              <img className="optionButton" alt="Settings" src={settingsIcon}/>
+              <img className="optionButton" alt="Viewer List" src={peopleIcon}/>
+            </div>
+            <div className="chatUserDisplay">
+              <p style={{color: "#ED0000", fontFamily: "Open Sans", margin: 0}}>Phineas</p>
+              <img style={{width: 18, marginLeft: 5}} alt="Admin" src={adminBadge}/>
+            </div>
           </div>
         </div>
-      </div>
+        {this.state.emotesVisible ? <EmoteMenu/> : null}
+      </Fragment>
     );
   }
 }
